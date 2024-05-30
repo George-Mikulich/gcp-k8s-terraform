16b6ab922eec8099ca0616111c6db90c1a2582f3
George-Mikulich <g.mikulich@softteco.com>
2024-05-30 14:43:02 +0300

    remove autoscaling

--- gke.tf
+++ gke.tf
@@ -40,18 +40,14 @@ resource "google_container_cluster" "primary" {
   network    = google_compute_network.vpc.name
   subnetwork = google_compute_subnetwork.subnet.name
 
-  cluster_autoscaling {
-    enabled = true
-    resource_limits {
-      resource_type = "cpu"
-      minimum       = 1
-      maximum       = 1
-    }
-    resource_limits {
-      resource_type = "memory"
-      minimum       = 7
-      maximum       = 50
-    }
+  private_cluster_config {
+    master_ipv4_cidr_block  = "172.16.0.0/28"
+    enable_private_endpoint = true
+    enable_private_nodes    = true
+  }
+  ip_allocation_policy {
+  }
+  master_authorized_networks_config {
   }
 }
 
