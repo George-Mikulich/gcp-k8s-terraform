33336a52831e854f3b538ea8b987951ba83b066f
George-Mikulich <g.mikulich@softteco.com>
2024-06-06 12:49:47 +0300

    add argocd and flask-app

--- gke.tf
+++ gke.tf
@@ -112,7 +112,6 @@ resource "google_container_node_pool" "public_nodes" {
   }
 }
 
-
 # # Kubernetes provider
 # # The Terraform Kubernetes Provider configuration below is used as a learning reference only. 
 # # It references the variables and resources provisioned in this file. 
@@ -120,14 +119,17 @@ resource "google_container_node_pool" "public_nodes" {
 # # https://learn.hashicorp.com/terraform/kubernetes/provision-gke-cluster#optional-configure-terraform-kubernetes-provider
 # # To learn how to schedule deployments and services using the provider, go here: https://learn.hashicorp.com/tutorials/terraform/kubernetes-provider.
 
-# provider "kubernetes" {
-#   load_config_file = "false"
+data "google_client_config" "current" {}
 
-#   host     = google_container_cluster.primary.endpoint
-#   username = var.gke_username
-#   password = var.gke_password
+provider "kubernetes" {
+  load_config_file = "false"
 
-#   client_certificate     = google_container_cluster.primary.master_auth.0.client_certificate
-#   client_key             = google_container_cluster.primary.master_auth.0.client_key
-#   cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
-# }
+  host     = google_container_cluster.primary.endpoint
+  username = var.gke_username
+  password = var.gke_password
+
+  client_certificate     = google_container_cluster.primary.master_auth.0.client_certificate
+  client_key             = google_container_cluster.primary.master_auth.0.client_key
+  cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
+  token                  = data.google_client_config.current.access_token
+}
