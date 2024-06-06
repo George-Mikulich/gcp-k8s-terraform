f9c38a5eef116b33917368c120a8fdcbd5fa187b
George-Mikulich <g.mikulich@softteco.com>
2024-06-06 13:33:36 +0300

    fix app.yaml

--- argocd.tf
+++ argocd.tf
@@ -1,19 +1,19 @@
-# provider "helm" {
-#   kubernetes {
-#     config_path = "~/.kube/config" ## issues? (old config may be loaded)
-#   }
-# }
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config" ## issues? (old config may be loaded)
+  }
+}
 
-# resource "helm_release" "argocd" {
-#   name  = "argocd"
+resource "helm_release" "argocd" {
+  name  = "argocd"
 
-#   repository       = "https://argoproj.github.io/argo-helm"
-#   chart            = "argo-cd"
-#   namespace        = "argocd"
-#   version          = "7.1.1"
-#   create_namespace = true
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  version          = "7.1.1"
+  create_namespace = true
 
-#   values = [
-#     file("argocd/app.yaml")
-#   ]
-# }
+  values = [
+    file("argocd/app.yaml")
+  ]
+}
