d7f2b3686d08b68950d7d065f35518358960793e
George-Mikulich <g.mikulich@softteco.com>
2024-06-06 16:30:31 +0300

    change tag

--- argocd.tf
+++ argocd.tf
@@ -12,7 +12,16 @@ resource "helm_release" "argocd" {
   namespace        = "argocd"
   version          = "7.1.1"
   create_namespace = true
+}
 
+resource "helm_release" "argocd_app" {
+  name = "argocd-app"
+
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argocd-apps"
+  namespace        = "argocd"
+  version          = "2.0.0"
+  
   values = [
     file("argocd/app.yaml")
   ]
